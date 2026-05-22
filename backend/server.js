f972160bcaa15e34baef0489a03c4e911e054b19
Manish12006 <ghanshanimanish152@gmail.com>
2026-05-22 16:43:28 +0530

    Configured Notes Routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const notesRoutes = require("./routes/nodeRoutes");
+
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("Mongoose connected"))
 .catch((err)=> console.log(err))
@@ -11,6 +13,7 @@ app.get("/", (req,res)=>{
     res.send("Backend Running!")
 })
 
+app.use("/notes", notesRoutes);
 app.listen(5000, () => {
     console.log("Server running on port 5000");
 })
